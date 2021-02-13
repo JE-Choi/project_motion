cd6f06708c78807b12417e6e4d282e4f04901005
최지은 <lielieing@naver.com>
2021-02-14 00:04:55 +0900

    BaseComponent상속받아서 image, note, todo 컴포넌트 생성

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
+import { TodoComponent } from "./components/page/item/todo.js";
 import { PageComponent } from "./components/page/page.js";
 import { ImageComponent } from "./components/page/item/image.js";
+import { NoteComponent } from "./components/page/item/note.js";
 class App {
   private readonly image: ImageComponent;
   private readonly page: PageComponent;
+  private readonly note: NoteComponent;
+  private readonly todo: TodoComponent;
 
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
@@ -12,6 +16,10 @@ class App {
       "https://picsum.photos/600/300"
     );
     this.image.attachTo(appRoot, "beforeend");
+    this.note = new NoteComponent("title", "noteBody");
+    this.note.attachTo(appRoot, "beforeend");
+    this.todo = new TodoComponent("todoTitle", "todoBody");
+    this.todo.attachTo(appRoot, "beforeend");
   }
 }
 
